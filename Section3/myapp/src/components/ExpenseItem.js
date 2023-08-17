ae3136aa06e6e5fac9a9002f3fd0224aaeb24641
Carlos Akión Garro Campos <48141593+AkionGarro@users.noreply.github.com>
2023-08-17 00:03:28 -0600

    Update

--- Section3/myapp/src/components/ExpenseItem.js
+++ Section3/myapp/src/components/ExpenseItem.js
@@ -5,14 +5,19 @@ function ExpenseItem(props) {
   return (
     <Card className="expense-item">
       <div>
-        <ExpenseDate 
-        date = {props.date}
-        ></ExpenseDate>
+        <ExpenseDate date={props.date}></ExpenseDate>
       </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button
+        onClick={() => {
+          console.log("clicked");
+        }}
+      >
+        Change title
+      </button>
     </Card>
   );
 }
