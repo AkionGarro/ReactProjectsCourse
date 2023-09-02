00b3c996945b1eeecb596c43ab2cc818ec89a68f
Carlos Akión Garro Campos <48141593+AkionGarro@users.noreply.github.com>
2023-09-01 23:51:47 -0600

    Update

--- Section3/myapp/src/App.js
+++ Section3/myapp/src/App.js
@@ -23,9 +23,15 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+
+  const addExpenseHandler = (expense) =>{
+    console.log("In App.js");
+    console.log(expense);
+  }
+
   return (
     <div>
-      <NewExpense></NewExpense>
+      <NewExpense onSaveExpense = {addExpenseHandler}></NewExpense>
       <Expenses items={expenses}></Expenses>
     </div>
   );
--- Section3/myapp/src/components/NewExpense/NewExpense.js
+++ Section3/myapp/src/components/NewExpense/NewExpense.js
@@ -2,10 +2,21 @@ import React, { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
-function NewExpense() {
+function NewExpense(props) {
+
+  const saveExpense = (enteredExpenseData) =>{
+    const expenseData = {
+      ...enteredExpenseData,
+      id : Math.random().toString()
+    };
+
+    props.onSaveExpense(expenseData);
+
+  }
+
   return (
     <div className="new-expense">
-        <ExpenseForm></ExpenseForm>
+        <ExpenseForm onSaveExpenseData={saveExpense}></ExpenseForm>
     </div>
   );
 }
